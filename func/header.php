e822827630ff9390f8a6104715a33671dfb74815
tukiyo3 <tukiyo3@gmail.com>
2020-09-22 11:11:29 +0900

    中央揃え

--- func/header.php
+++ func/header.php
@@ -2,12 +2,12 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 	<style>
 	body {
-		max-width: 640px;
+		max-width: 800px;
+		margin: 0 auto;
 	}
 	</style>
 </head>
